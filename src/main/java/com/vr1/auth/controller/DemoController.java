a9bb9bb40642f6a6b02ceb07444b10b816409731
Gogul11 <gogul0811@gmail.com>
2025-10-28 17:43:27 +0530

    structure

---
+++ src/main/java/com/vr1/auth/controller/DemoController.java
@@ -0,0 +1,14 @@
+package com.vr1.auth.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/restDemo")
+public class DemoController{
+    @GetMapping("/")
+    public String demo(){
+        return "Welcome to Rest api demo";
+    }
+}
